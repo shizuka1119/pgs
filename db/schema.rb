ee3553011e9cbb7bece48967d29fa2dac2f9bba3
shizuka <shizuka1119@gmail.com>
2020-03-21 06:19:37 +0000

    admin add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_082819) do
+ActiveRecord::Schema.define(version: 2020_03_21_055538) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "profile_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.integer "user_id"
