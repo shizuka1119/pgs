c1baeb3b12a73650661fc9b7f95f06f562723829
shizuka <shizuka1119@gmail.com>
2020-03-28 14:53:25 +0000

    Active Storage add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,12 @@ class User < ApplicationRecord
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
   has_many :items, :dependent => :destroy
-
   has_many :relationships, :dependent => :destroy
-
   has_many :followings, through: :relationships, source: :follow, :dependent => :destroy
-
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', :dependent => :destroy
-
   has_many :followers, through: :reverse_of_relationships, source: :user, :dependent => :destroy
 
+  has_one_attached :profile_image
 
   def follow(other_user)
     unless self == other_user
